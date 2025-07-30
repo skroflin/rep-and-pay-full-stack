7a39333e7ed0addaf623f241279f11d1fca51b6c
skroflin <99004305+skroflin@users.noreply.github.com>
2025-07-30 15:38:58 +0200

    Napravljene metode za ažuriranje i brisanje
    Napravljene metode za ažuriranje (put) i brisanje (delete) za BookingController.

--- server-side/src/main/java/fina/skroflin/controller/BookingController.java
+++ server-side/src/main/java/fina/skroflin/controller/BookingController.java
@@ -13,8 +13,10 @@ import jakarta.persistence.NoResultException;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +30,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("/api/fina/skroflin/booking")
 public class BookingController {
+
     private final BookingService bookingService;
     private final UserService userService;
     private final TrainingSessionService trainingSessionService;
@@ -37,12 +40,12 @@ public class BookingController {
         this.userService = userService;
         this.trainingSessionService = trainingSessionService;
     }
-    
+
     @GetMapping("/get")
-    public ResponseEntity<List<BookingResponseDTO>> getAll(){
+    public ResponseEntity<List<BookingResponseDTO>> getAll() {
         try {
             return new ResponseEntity<>(
-                    bookingService.getAll(),HttpStatus.OK
+                    bookingService.getAll(), HttpStatus.OK
             );
         } catch (Exception e) {
             throw new ResponseStatusException(
@@ -52,22 +55,22 @@ public class BookingController {
             );
         }
     }
-    
+
     @GetMapping("/getById")
     public ResponseEntity<BookingResponseDTO> getById(
             @RequestParam int id
-    ){
+    ) {
         try {
             if (id <= 0) {
                 throw new ResponseStatusException(
-                        HttpStatus.BAD_REQUEST, 
+                        HttpStatus.BAD_REQUEST,
                         "Id musn't be lesser than 0"
                 );
             }
             BookingResponseDTO booking = bookingService.getById(id);
             if (booking == null) {
                 throw new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, 
+                        HttpStatus.NOT_FOUND,
                         "Booking with the id" + " " + id + " " + "doesn't exist!"
                 );
             }
@@ -76,21 +79,20 @@ public class BookingController {
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR,
                     "Error upon fetching booking with id"
-                            + " " + id + " " + e.getMessage(),
+                    + " " + id + " " + e.getMessage(),
                     e
             );
         }
     }
-    
+
     @PostMapping("/post")
     public ResponseEntity<BookingResponseDTO> post(
-            @RequestBody(required = true)
-            BookingDTO dto
-    ){
+            @RequestBody(required = true) BookingDTO dto
+    ) {
         try {
             if (dto == null) {
                 throw new ResponseStatusException(
-                        HttpStatus.BAD_REQUEST, 
+                        HttpStatus.BAD_REQUEST,
                         "The necessary data wasn't inserted!"
                 );
             }
@@ -100,9 +102,9 @@ public class BookingController {
                 } catch (Exception e) {
                     throw new ResponseStatusException(
                             HttpStatus.NOT_FOUND,
-                            "Error user with id" 
-                                    + " " + dto.userId() 
-                                    + " " + "doesn't exist"
+                            "Error user with id"
+                            + " " + dto.userId()
+                            + " " + "doesn't exist"
                     );
                 }
             }
@@ -112,38 +114,135 @@ public class BookingController {
                 } catch (Exception e) {
                     throw new ResponseStatusException(
                             HttpStatus.NOT_FOUND,
-                            "Error training session with id" 
-                                    + " " + dto.userId() 
-                                    + " " + "doesn't exist"
+                            "Error training session with id"
+                            + " " + dto.userId()
+                            + " " + "doesn't exist"
                     );
                 }
             }
-            if (dto.reservationTime() == null || 
-                    dto.reservationTime().equals(null)) {
-               throw new ResponseStatusException(
-                       HttpStatus.BAD_REQUEST,
-                       "Reservation time is necessary!"
-               );
+            if (dto.reservationTime() == null
+                    || dto.reservationTime().equals(null)) {
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST,
+                        "Reservation time is necessary!"
+                );
             }
-            
+
             BookingResponseDTO createdBooking = bookingService.post(dto);
             return new ResponseEntity<>(createdBooking, HttpStatus.CREATED);
         } catch (IllegalArgumentException e) {
             throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, 
-                    e.getMessage(), 
+                    HttpStatus.BAD_REQUEST,
+                    e.getMessage(),
                     e
             );
         } catch (NoResultException e) {
             throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, 
-                    e.getMessage(), 
+                    HttpStatus.NOT_FOUND,
+                    e.getMessage(),
                     e
             );
         } catch (Exception e) {
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR,
-                    e.getMessage(), 
+                    e.getMessage(),
+                    e
+            );
+        }
+    }
+
+    @PutMapping("/put")
+    public ResponseEntity<BookingResponseDTO> put(
+            @RequestParam int id,
+            @RequestBody(required = true) BookingDTO dto
+    ) {
+        try {
+            if (id <= 0) {
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST,
+                        "Id musn't be lesser than 0"
+                );
+            }
+            if (dto.userId() != null) {
+                try {
+                    userService.getById(dto.userId());
+                } catch (Exception e) {
+                    throw new ResponseStatusException(
+                            HttpStatus.NOT_FOUND,
+                            "Error user with id"
+                            + " " + dto.userId()
+                            + " " + "doesn't exist"
+                    );
+                }
+            }
+            if (dto.trainingSessionId() != null) {
+                try {
+                    trainingSessionService.getById(dto.trainingSessionId());
+                } catch (Exception e) {
+                    throw new ResponseStatusException(
+                            HttpStatus.NOT_FOUND,
+                            "Error training session with id"
+                            + " " + dto.userId()
+                            + " " + "doesn't exist"
+                    );
+                }
+            }
+            if (dto.reservationTime() == null
+                    || dto.reservationTime().equals(null)) {
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST,
+                        "Reservation time is necessary!"
+                );
+            }
+            BookingResponseDTO updatedBooking = bookingService.put(dto, id);
+            return new ResponseEntity<>(updatedBooking, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    e.getMessage(),
+                    e
+            );
+        } catch (NoResultException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    e.getMessage(),
+                    e
+            );
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    e.getMessage(),
+                    e
+            );
+        }
+    }
+    
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> delete(
+            @RequestParam int id
+    ){
+        try {
+            if (id <= 0) {
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST, 
+                        "Id musn't be lesser than 0!"
+                );
+            }
+            bookingService.delete(id);
+            return new ResponseEntity<>(
+                    "Booking with id" 
+                            + " " + id 
+                            + " " + "deleted", 
+                    HttpStatus.OK
+            );
+        } catch (NoResultException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Error upon deletion" + " " + e.getMessage(),
                     e
             );
         }
