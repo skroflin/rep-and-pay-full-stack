94e2255fd86aecb8b09d396f2305f5c2e9b39918
skroflin <99004305+skroflin@users.noreply.github.com>
2025-08-04 15:12:32 +0200

    Izrada metoda za API zahtjeve
    Izrada metoda za slanje API zahtjeva na `api.ts`.
    Potrebno kreirati zasebne metode koje će slati API zahtjeve koji se odnose na autentificirane korisnike = korisnik koji je trenutno u sesiji.

--- client-side/src/utils/api.ts
+++ client-side/src/utils/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 import { getAuthToken } from "./helper";
+import type { UserRequest } from "./types/User";
+import type { RegistrationUserRequest } from "./types/Register";
+import type { LoginUserRequest } from "./types/Login";
+import type { BookingRequest } from "./types/Booking";
+import type { TrainingSessionRequest } from "./types/TrainingSession";
 
 const BASE_URL = 'http://localhost:8080'
 
@@ -66,3 +71,25 @@ async function apiDeleteCall(
     )
     return res.data
 }
+
+//user methods
+export const getUsers = () => apiGetCall("user/get")
+export const getUserById = (userId: string) => apiGetCall(`user/getById?id=${userId}`)
+export const registerUser = (req: RegistrationUserRequest) => apiPostCall<RegistrationUserRequest>("user/register", req)
+export const loginUser = (req: LoginUserRequest) => apiPostCall<LoginUserRequest>("user/login", req)
+export const updateUser = (req: UserRequest) => apiPutCall<UserRequest>("/user/put", req)
+export const deleteUser = (userId: string) => apiDeleteCall(`user/delete?id=${userId}`)
+
+//booking methods
+export const getBookings = () => apiGetCall("booking/get")
+export const getBookingById = (bookingId: string) => apiGetCall(`booking/getById?id=${bookingId}`)
+export const createBooking = (req: BookingRequest) => apiPostCall<BookingRequest>("booking/post", req)
+export const updateBooking = (req: BookingRequest) => apiPutCall<BookingRequest>("booking/put", req)
+export const deleteBooking = (bookingId: string) => apiDeleteCall(`booking/delete?id=${bookingId}`)
+
+//training session methods
+export const getTrainingSessions = () => apiGetCall("trainingSessions/get")
+export const getTrainingSessionById = (trainingSessionId: string) => apiGetCall(`trainingSession/getById?id=${trainingSessionId}`)
+export const createTrainingSession = (req: TrainingSessionRequest) => apiPostCall<TrainingSessionRequest>("trainingSession/post", req)
+export const updateTrainingSession = (req: TrainingSessionRequest) => apiPutCall<TrainingSessionRequest>("trainingSession/put", req)
+export const deleteTrainingSession = (trainingsessionId: string) => apiDeleteCall(`trainingSession/delete?id=${trainingsessionId}`) 
