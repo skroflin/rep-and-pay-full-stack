2ac4d03dce5579f264cbf537509d00c69b56dec9
Sven Kroflin <99004305+skroflin@users.noreply.github.com>
2025-08-05 12:35:35 +0200

    Prepravak helper.ts-a i završene metode za api.ts
    Prepravljen (nadograđen) helper.ts za dohvaćanje korisničkog imena i uloge.
    Dodane nove metode unutar api.ts namijenjene za dohvaćanje, izradu, ažuriranje i brisanje podataka korisnika koji su trenutno u sesiji (authenticated).

--- client-side/src/utils/api.ts
+++ client-side/src/utils/api.ts
@@ -5,12 +5,15 @@ import type { RegistrationUserRequest } from "./types/Register";
 import type { LoginUserRequest } from "./types/Login";
 import type { BookingRequest } from "./types/Booking";
 import type { TrainingSessionRequest } from "./types/TrainingSession";
+import type { PasswordUserRequest } from "./types/Password";
+import type { MyTrainingSessionRequest } from "./types/user-authenticated/MyTrainingSession";
+import type { MyBookingRequest } from "./types/user-authenticated/MyBooking";
 
 const BASE_URL = 'http://localhost:8080'
 
 async function apiGetCall(
     route: string
-){
+) {
     const res = await axios.get(
         `${BASE_URL}/api/fina/skroflin/${route}`,
         {
@@ -24,27 +27,10 @@ async function apiGetCall(
 }
 
 async function apiPostCall<Req>(
-    route: string, 
-    data: Req
-) {
-    const res = await axios.post(
-        `${BASE_URL}/api/fina/skroflin/${route}`,
-        data,
-        {
-           headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Authorization": `Bearer ${getAuthToken}`
-            } 
-        }
-    )    
-    return res.data
-}
-
-async function apiPutCall<Req>(
     route: string,
     data: Req
 ) {
-   const res = await axios.put(
+    const res = await axios.post(
         `${BASE_URL}/api/fina/skroflin/${route}`,
         data,
         {
@@ -53,8 +39,25 @@ async function apiPutCall<Req>(
                 "Authorization": `Bearer ${getAuthToken}`
             }
         }
-   )
-   return res.data 
+    )
+    return res.data
+}
+
+async function apiPutCall<Req>(
+    route: string,
+    data: Req
+) {
+    const res = await axios.put(
+        `${BASE_URL}/api/fina/skroflin/${route}`,
+        data,
+        {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Authorization": `Bearer ${getAuthToken}`
+            }
+        }
+    )
+    return res.data
 }
 
 async function apiDeleteCall(
@@ -77,19 +80,33 @@ export const getUsers = () => apiGetCall("user/get")
 export const getUserById = (userId: string) => apiGetCall(`user/getById?id=${userId}`)
 export const registerUser = (req: RegistrationUserRequest) => apiPostCall<RegistrationUserRequest>("user/register", req)
 export const loginUser = (req: LoginUserRequest) => apiPostCall<LoginUserRequest>("user/login", req)
-export const updateUser = (req: UserRequest) => apiPutCall<UserRequest>("/user/put", req)
+export const updateUser = (req: UserRequest, userId: string) => apiPutCall<UserRequest>(`user/put?id=${userId}`, req)
 export const deleteUser = (userId: string) => apiDeleteCall(`user/delete?id=${userId}`)
+export const changeUserPassword = (req: PasswordUserRequest) => apiPutCall<PasswordUserRequest>("user/changePassword", req)
 
 //booking methods
 export const getBookings = () => apiGetCall("booking/get")
 export const getBookingById = (bookingId: string) => apiGetCall(`booking/getById?id=${bookingId}`)
 export const createBooking = (req: BookingRequest) => apiPostCall<BookingRequest>("booking/post", req)
-export const updateBooking = (req: BookingRequest) => apiPutCall<BookingRequest>("booking/put", req)
+export const updateBooking = (req: BookingRequest, bookingId: string) => apiPutCall<BookingRequest>(`booking/put?=${bookingId}`, req)
 export const deleteBooking = (bookingId: string) => apiDeleteCall(`booking/delete?id=${bookingId}`)
 
 //training session methods
 export const getTrainingSessions = () => apiGetCall("trainingSessions/get")
 export const getTrainingSessionById = (trainingSessionId: string) => apiGetCall(`trainingSession/getById?id=${trainingSessionId}`)
 export const createTrainingSession = (req: TrainingSessionRequest) => apiPostCall<TrainingSessionRequest>("trainingSession/post", req)
-export const updateTrainingSession = (req: TrainingSessionRequest) => apiPutCall<TrainingSessionRequest>("trainingSession/put", req)
-export const deleteTrainingSession = (trainingsessionId: string) => apiDeleteCall(`trainingSession/delete?id=${trainingsessionId}`) 
+export const updateTrainingSession = (req: TrainingSessionRequest, trainingSessionId: string) => apiPutCall<TrainingSessionRequest>(`trainingSession/put?=${trainingSessionId}`, req)
+export const deleteTrainingSession = (trainingsessionId: string) => apiDeleteCall(`trainingSession/delete?id=${trainingsessionId}`)
+
+//user-authenticated methods (user, trainingSesions and bookings)
+export const getMyProfile = () => apiGetCall("user/getMyProfile")
+export const updateMyProfile = (req: UserRequest) => apiPutCall<UserRequest>("user/updateMyProfile", req)
+export const deleteMyProfile = () => apiDeleteCall("user/deleteMyProfile")
+export const getMyTrainingSessions = () => apiGetCall("trainingSession/getMyTrainingSessions")
+export const createMyTrainingSession = (req: MyTrainingSessionRequest) => apiPostCall<MyTrainingSessionRequest>("trainingSession/createMyTrainingSession", req)
+export const updateMyTrainingSession = (req: MyTrainingSessionRequest, myTrainingSessionId: string) => apiPutCall<MyTrainingSessionRequest>(`trainingSession/updateMyTrainingSession?id=${myTrainingSessionId}`, req)
+export const deleteMyTrainingSession = (myTrainingSession: string) => apiDeleteCall(`trainingSession/deleteMyTrainingSession?id=${myTrainingSession}`)
+export const getMyBookings = () => apiGetCall("booking/getMyBookings")
+export const createMyBooking = (req: MyBookingRequest) => apiPostCall<MyBookingRequest>("booking/createMyBooking", req)
+export const updateMyBooking = (req: MyBookingRequest, myBookingId: string) => apiPutCall<MyBookingRequest>(`booking/updateMyBooking?id=${myBookingId}`, req)
+export const deleteMyBooking = (myBookingId: string) => apiDeleteCall(`booking/deleteMyBooking?id=${myBookingId}`)
--- client-side/src/utils/helper.ts
+++ client-side/src/utils/helper.ts
@@ -1,8 +1,8 @@
 export const isUserAuthorized = () => {
-    const token = localStorage.getItem("jwt")
-    const role = localStorage.getItem("role")
-    const username = localStorage.getItem("username")
-    return !!token && !!role && !!username
+    const token = !!localStorage.getItem("jwt")
+    const role = !!localStorage.getItem("role")
+    const username = !!localStorage.getItem("username")
+    return token && role && username
 }
 
 export function getAuthToken() {
@@ -13,6 +13,24 @@ export function setAuthToken(token: string) {
     localStorage.setItem("jwt", token)
 }
 
+export function getUsername() {
+    return localStorage.getItem("username") || ""
+}
+
+export function setUsername(username: string) {
+    return localStorage.setItem("username", username)
+}
+
+export function getRole() {
+    return localStorage.getItem("role") || ""
+}
+
+export function setRole(role: string) {
+    return localStorage.setItem("role", role)
+}
+
 export function clearAuthToken() {
     localStorage.removeItem("jwt")
+    localStorage.removeItem("username")
+    localStorage.removeItem("role")
 }
