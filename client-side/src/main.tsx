df5f2ed63441da4197c335b37f5e612ac9597572
skroflin <99004305+skroflin@users.noreply.github.com>
2025-08-06 15:21:26 +0200

    Prepravak

--- client-side/src/main.tsx
+++ client-side/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router'
 import { UserProvider } from './user-context/User.tsx'
+import { AllRoutes } from './routes.tsx'
 
 const queryClient = new QueryClient()
 
@@ -14,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <UserProvider>
-        <App />
+          <AllRoutes />
         </UserProvider>
       </QueryClientProvider>
     </BrowserRouter>
