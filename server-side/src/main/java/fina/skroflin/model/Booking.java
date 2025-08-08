290cbab8b60c99f0092e4f21222760361c39db30
skroflin <99004305+skroflin@users.noreply.github.com>
2025-08-08 13:10:15 +0200

    Dodatak

--- server-side/src/main/java/fina/skroflin/model/Booking.java
+++ server-side/src/main/java/fina/skroflin/model/Booking.java
@@ -8,6 +8,7 @@ import fina.skroflin.model.enums.BookingStatus;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
